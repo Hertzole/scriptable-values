7d9c1afa7f9203a556dbd7cbbeb53696fd75dabf
Hertzole <hampusringstrom@gmail.com>
2022-10-19 18:26:14 +0200

    add: equality helper tests

---
+++ Packages/com.aurorapunks.scriptablevalues/Tests/Editor/EqualityHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using AuroraPunks.ScriptableValues.Helpers;
+using NUnit.Framework;
+using UnityEngine;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace AuroraPunks.ScriptableValues.Tests.Editor
+{
+	public class EqualityHelperTests
+	{
+		[Test]
+		public void NonUnityObject_Equals()
+		{
+			Assert.IsTrue(EqualityHelper.Equals(0, 0));
+		}
+		
+		[Test]
+		public void NonUnityObject_NotEquals()
+		{
+			Assert.IsFalse(EqualityHelper.Equals(0, 1));
+		}
+		[Test]
+		public void UnityObject_Equals()
+		{
+			GameObject go = new GameObject();
+			
+			Assert.IsTrue(EqualityHelper.Equals(go, go));
+
+			Object.DestroyImmediate(go);
+		}
+		
+		[Test]
+		public void UnityObject_NotEquals()
+		{
+			GameObject go1 = new GameObject();
+			GameObject go2 = new GameObject();
+			
+			Assert.IsFalse(EqualityHelper.Equals(go1, go2));
+			
+			Object.DestroyImmediate(go1);
+			Object.DestroyImmediate(go2);
+		}
+	}
+}
