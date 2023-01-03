44de8bfd0c6a2c4fcfef9daa38ec1066bbf330e4
Hertzole <hampusringstrom@gmail.com>
2023-01-03 06:16:26 +0100

    fix: made game object pool just as functional as component pool

--- Packages/com.aurorapunks.scriptablevalues/Runtime/Pools/ScriptableGameObjectPool.cs
+++ Packages/com.aurorapunks.scriptablevalues/Runtime/Pools/ScriptableGameObjectPool.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace AuroraPunks.ScriptableValues
 {
@@ -7,9 +8,13 @@ namespace AuroraPunks.ScriptableValues
 #endif
 	public class ScriptableGameObjectPool : ScriptablePool<GameObject>
 	{
-		[SerializeField] 
+		[SerializeField]
 		private GameObject prefab = default;
-		
+
+		private readonly List<IPoolable> poolableBuffer = new List<IPoolable>(10);
+
+		public GameObject Prefab { get { return prefab; } set { prefab = value; } }
+
 		protected override GameObject CreateObject()
 		{
 			return Instantiate(prefab);
@@ -17,7 +22,53 @@ namespace AuroraPunks.ScriptableValues
 
 		protected override void DestroyObject(GameObject item)
 		{
-			Destroy(item);
+			// The object may already have been destroyed. Check if it exists first.
+			if (item != null)
+			{
+				Destroy(item.gameObject);
+			}
+		}
+
+		internal override void OnGetInternal(GameObject item)
+		{
+			// Don't check for poolable here and it's done with components in OnGet.
+			OnGet(item);
+		}
+
+		internal override void OnReturnInternal(GameObject item)
+		{
+			// Don't check for poolable here and it's done with components in OnReturn.
+			OnReturn(item);
+		}
+
+		protected override void OnGet(GameObject item)
+		{
+			item.gameObject.SetActive(true);
+
+			poolableBuffer.Clear();
+			item.GetComponentsInChildren(true, poolableBuffer);
+			for (int i = 0; i < poolableBuffer.Count; i++)
+			{
+				poolableBuffer[i].OnUnpooled();
+			}
+		}
+
+		protected override void OnReturn(GameObject item)
+		{
+			if (item == null)
+			{
+				return;
+			}
+
+			poolableBuffer.Clear();
+			item.GetComponentsInChildren(true, poolableBuffer);
+
+			for (int i = 0; i < poolableBuffer.Count; i++)
+			{
+				poolableBuffer[i].OnPooled();
+			}
+
+			item.gameObject.SetActive(false);
 		}
 	}
 }
