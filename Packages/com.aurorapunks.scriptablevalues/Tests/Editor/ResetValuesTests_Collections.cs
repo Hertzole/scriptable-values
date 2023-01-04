a682a3ec5fc0cc384cac2497e6ee586621b9fc81
Hertzole <hampusringstrom@gmail.com>
2023-01-04 01:49:14 +0100

    add: dictionary reset tests

---
+++ Packages/com.aurorapunks.scriptablevalues/Tests/Editor/ResetValuesTests_Collections.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace AuroraPunks.ScriptableValues.Tests.Editor
+{
+	public partial class ResetValuesTests
+	{
+		[Test]
+		public void Dictionary_ResetEvents()
+		{
+			TestScriptableDictionary instance = CreateInstance<TestScriptableDictionary>();
+
+			bool addedWasInvoked = false;
+			bool removedWasInvoked = false;
+			bool clearedWasInvoked = false;
+			bool setWasInvoked = false;
+
+			instance.OnAdded += (_, _) => { addedWasInvoked = true; };
+			instance.OnRemoved += (_, _) => { removedWasInvoked = true; };
+			instance.OnCleared += () => { clearedWasInvoked = true; };
+			instance.OnSet += (_, _, _) => { setWasInvoked = true; };
+
+			instance.ResetValues();
+
+			instance.Add(0, 1);
+			instance.Remove(0);
+			instance.Clear();
+			instance[0] = 1;
+
+			Assert.IsFalse(addedWasInvoked);
+			Assert.IsFalse(removedWasInvoked);
+			Assert.IsFalse(clearedWasInvoked);
+			Assert.IsFalse(setWasInvoked);
+		}
+
+		[Test]
+		public void Dictionary_ResetValues([ValueSource(nameof(bools))] bool isReadOnly, [ValueSource(nameof(bools))] bool clearOnStart)
+		{
+			TestScriptableDictionary instance = CreateInstance<TestScriptableDictionary>();
+
+			instance.Add(0, 1);
+			instance.Add(2, 3);
+			instance.Add(4, 5);
+			
+			instance.IsReadOnly = isReadOnly;
+			instance.ClearOnStart = clearOnStart;
+
+			instance.ResetValues();
+
+			if (!isReadOnly && clearOnStart)
+			{
+				Assert.AreEqual(0, instance.Count);
+				Assert.AreEqual(0, instance.dictionary.Count);
+				Assert.AreEqual(0, instance.values.Count);
+				Assert.AreEqual(0, instance.keys.Count);
+			}
+			else
+			{
+				Assert.AreEqual(3, instance.Count);
+				Assert.AreEqual(3, instance.dictionary.Count);
+				Assert.AreEqual(3, instance.values.Count);
+				Assert.AreEqual(3, instance.keys.Count);
+			}
+		}
+	}
+}
