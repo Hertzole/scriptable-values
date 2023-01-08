7ca5578c70959861120d8a702bb9cffc37862e39
Hertzole <hampusringstrom@gmail.com>
2023-01-08 13:49:08 +0100

    add: tests for checking add stack trace preference

--- Packages/com.aurorapunks.scriptablevalues/Tests/Editor/StackTracesTest.cs
+++ Packages/com.aurorapunks.scriptablevalues/Tests/Editor/StackTracesTest.cs
@@ -1,160 +1,148 @@
 ﻿using System;
 using System.Collections.Generic;
 using AuroraPunks.ScriptableValues.Debugging;
+using AuroraPunks.ScriptableValues.Editor;
 using NUnit.Framework;
 using UnityEngine;
 using Assert = UnityEngine.Assertions.Assert;
-using Object = UnityEngine.Object;
 
 namespace AuroraPunks.ScriptableValues.Tests.Editor
 {
-	public class StackTracesTest
+	public class StackTracesTest : BaseEditorTest
 	{
+		private bool originalCollectStackTraces;
 		private const int MAX_STACK_TRACE_LENGTH = IStackTraceProvider.MAX_STACK_TRACE_ENTRIES;
 
+		protected override void OnSetup()
+		{
+			base.OnSetup();
+
+			originalCollectStackTraces = ScriptableValuesPreferences.CollectStackTraces;
+		}
+
+		protected override void OnTearDown()
+		{
+			base.OnTearDown();
+
+			ScriptableValuesPreferences.CollectStackTraces = originalCollectStackTraces;
+		}
+
 		// Scriptable Values
-		
+
 		[Test]
 		public void MaxStackTraces_ScriptableValues()
 		{
-			TestStackTraceCount<ScriptableInt>(i =>
-			{
-				i.Value++;
-			});
+			TestStackTraceCount<ScriptableInt>(i => { i.Value++; });
 		}
 
 		[Test]
 		public void StackTraceEventInvoked_ScriptableValues_SetValue()
 		{
-			TestStackTraceEventInvoked<ScriptableInt>(null, i =>
-			{
-				i.Value = 1;
-			});
+			TestStackTraceEventInvoked<ScriptableInt>(null, i => { i.Value = 1; });
 		}
 
 		[Test]
 		public void StackTracesReset_ScriptableValues()
 		{
-			TestStackTracesWereReset<ScriptableInt>(i =>
-			{
-				i.Value = 1;
-			});
+			TestStackTracesWereReset<ScriptableInt>(i => { i.Value = 1; });
 		}
 
 		[Test]
 		public void StackTrace_DontCollect_ScriptableValues()
 		{
-			TestStackTraceWereNotCollected<ScriptableInt>(i =>
-			{
-				i.Value = 1;
-			});
+			TestStackTraceWereNotCollected<ScriptableInt>(i => { i.Value = 1; });
 		}
-		
+
+		[Test]
+		public void StackTrace_DontCollect_Global_ScriptableValues()
+		{
+			TestStackTraceWereNoCollectGloballyCollected<ScriptableInt>(i => { i.Value = 1; });
+		}
+
 		// Events
-		
+
 		[Test]
 		public void MaxStackTraces_ScriptableEvent()
 		{
-			TestStackTraceCount<ScriptableEvent>(i =>
-			{
-				i.Invoke(this);
-			});
+			TestStackTraceCount<ScriptableEvent>(i => { i.Invoke(this); });
 		}
 
 		[Test]
 		public void StackTraceEventInvoked_ScriptableEvent_Invoke()
 		{
-			TestStackTraceEventInvoked<ScriptableEvent>(null, i =>
-			{
-				i.Invoke(this);
-			});
+			TestStackTraceEventInvoked<ScriptableEvent>(null, i => { i.Invoke(this); });
 		}
-		
+
 		[Test]
 		public void StackTracesReset_ScriptableEvent()
 		{
-			TestStackTracesWereReset<ScriptableEvent>(i =>
-			{
-				i.Invoke(this);
-			});
+			TestStackTracesWereReset<ScriptableEvent>(i => { i.Invoke(this); });
 		}
-		
+
 		[Test]
 		public void StackTrace_DontCollect_ScriptableEvent()
 		{
-			TestStackTraceWereNotCollected<ScriptableEvent>(i =>
-			{
-				i.Invoke(this);
-			});
+			TestStackTraceWereNotCollected<ScriptableEvent>(i => { i.Invoke(this); });
 		}
-		
+
+		[Test]
+		public void StackTrace_DontCollect_Globally_ScriptableEvent()
+		{
+			TestStackTraceWereNoCollectGloballyCollected<ScriptableEvent>(i => { i.Invoke(this); });
+		}
+
 		// Generic events
-		
+
 		[Test]
 		public void MaxStackTraces_ScriptableGenericEvent()
 		{
-			TestStackTraceCount<ScriptableIntEvent>(i =>
-			{
-				i.Invoke(this, 1);
-			});
+			TestStackTraceCount<ScriptableIntEvent>(i => { i.Invoke(this, 1); });
 		}
 
 		[Test]
 		public void StackTraceEventInvoked_ScriptableGenericEvent_Invoke()
 		{
-			TestStackTraceEventInvoked<ScriptableIntEvent>(null, i =>
-			{
-				i.Invoke(this, 1);
-			});
+			TestStackTraceEventInvoked<ScriptableIntEvent>(null, i => { i.Invoke(this, 1); });
 		}
-		
+
 		[Test]
 		public void StackTracesReset_ScriptableGenericEvent()
 		{
-			TestStackTracesWereReset<ScriptableIntEvent>(i =>
-			{
-				i.Invoke(this, 1);
-			});
+			TestStackTracesWereReset<ScriptableIntEvent>(i => { i.Invoke(this, 1); });
 		}
-		
+
 		[Test]
 		public void StackTrace_DontCollect_ScriptableGenericEvent()
 		{
-			TestStackTraceWereNotCollected<ScriptableIntEvent>(i =>
-			{
-				i.Invoke(this);
-			});
+			TestStackTraceWereNotCollected<ScriptableIntEvent>(i => { i.Invoke(this); });
 		}
-		
+
+		[Test]
+		public void StackTrace_DontCollect_Globally_ScriptableGenericEvent()
+		{
+			TestStackTraceWereNoCollectGloballyCollected<ScriptableIntEvent>(i => { i.Invoke(this); });
+		}
+
 		// List
-		
+
 		[Test]
 		public void MaxStackTraces_ScriptableList()
 		{
-			TestStackTraceCount<TestScriptableList>(i =>
-			{
-				i.Add(i.Count);
-			});
+			TestStackTraceCount<TestScriptableList>(i => { i.Add(i.Count); });
 		}
 
 		[Test]
 		public void StackTraceEventInvoked_ScriptableList_Add()
 		{
-			TestStackTraceEventInvoked<TestScriptableList>(null, i =>
-			{
-				i.Add(1);
-			});
+			TestStackTraceEventInvoked<TestScriptableList>(null, i => { i.Add(1); });
 		}
 
 		[Test]
 		public void StackTraceEventInvoked_ScriptableList_Insert()
 		{
-			TestStackTraceEventInvoked<TestScriptableList>(null, i =>
-			{
-				i.Insert(0, 1);
-			});
+			TestStackTraceEventInvoked<TestScriptableList>(null, i => { i.Insert(0, 1); });
 		}
-		
+
 		[Test]
 		public void StackTraceEventInvoked_ScriptableList_Remove()
 		{
@@ -165,12 +153,9 @@ namespace AuroraPunks.ScriptableValues.Tests.Editor
 				i.Add(3);
 				i.Add(4);
 				i.Add(5);
-			}, i =>
-			{
-				i.Remove(3);
-			});
+			}, i => { i.Remove(3); });
 		}
-		
+
 		[Test]
 		public void StackTraceEventInvoked_ScriptableList_RemoveAt()
 		{
@@ -181,36 +166,21 @@ namespace AuroraPunks.ScriptableValues.Tests.Editor
 				i.Add(3);
 				i.Add(4);
 				i.Add(5);
-			}, i =>
-			{
-				i.RemoveAt(0);
-			});
+			}, i => { i.RemoveAt(0); });
 		}
-		
+
 		[Test]
 		public void StackTraceEventInvoked_ScriptableList_Clear()
 		{
-			TestStackTraceEventInvoked<TestScriptableList>(i =>
-			{
-				i.Add(1);
-			}, i =>
-			{
-				i.Clear();
-			});
+			TestStackTraceEventInvoked<TestScriptableList>(i => { i.Add(1); }, i => { i.Clear(); });
 		}
-		
+
 		[Test]
 		public void StackTraceEventInvoked_ScriptableList_SetValue()
 		{
-			TestStackTraceEventInvoked<TestScriptableList>(i =>
-			{
-				i.Add(1);
-			}, i =>
-			{
-				i[0] = 42;
-			});
+			TestStackTraceEventInvoked<TestScriptableList>(i => { i.Add(1); }, i => { i[0] = 42; });
 		}
-		
+
 		[Test]
 		public void StackTraceEventInvoked_ScriptableList_Reverse()
 		{
@@ -221,11 +191,8 @@ namespace AuroraPunks.ScriptableValues.Tests.Editor
 				i.Add(3);
 				i.Add(4);
 				i.Add(5);
-			}, i =>
-			{
-				i.Reverse();
-			});
-			
+			}, i => { i.Reverse(); });
+
 			TestStackTraceEventInvoked<TestScriptableList>(i =>
 			{
 				i.Add(1);
@@ -233,12 +200,9 @@ namespace AuroraPunks.ScriptableValues.Tests.Editor
 				i.Add(3);
 				i.Add(4);
 				i.Add(5);
-			}, i =>
-			{
-				i.Reverse(0, i.Count);
-			});
+			}, i => { i.Reverse(0, i.Count); });
 		}
-		
+
 		[Test]
 		public void StackTraceEventInvoked_ScriptableList_Sort()
 		{
@@ -249,11 +213,8 @@ namespace AuroraPunks.ScriptableValues.Tests.Editor
 				i.Add(3);
 				i.Add(4);
 				i.Add(5);
-			}, i =>
-			{
-				i.Sort();
-			});
-			
+			}, i => { i.Sort(); });
+
 			TestStackTraceEventInvoked<TestScriptableList>(i =>
 			{
 				i.Add(1);
@@ -261,11 +222,8 @@ namespace AuroraPunks.ScriptableValues.Tests.Editor
 				i.Add(3);
 				i.Add(4);
 				i.Add(5);
-			}, i =>
-			{
-				i.Sort(Comparer<int>.Default);
-			});
-			
+			}, i => { i.Sort(Comparer<int>.Default); });
+
 			TestStackTraceEventInvoked<TestScriptableList>(i =>
 			{
 				i.Add(1);
@@ -273,11 +231,8 @@ namespace AuroraPunks.ScriptableValues.Tests.Editor
 				i.Add(3);
 				i.Add(4);
 				i.Add(5);
-			}, i =>
-			{
-				i.Sort((x, y) => x.CompareTo(y));
-			});
-			
+			}, i => { i.Sort((x, y) => x.CompareTo(y)); });
+
 			TestStackTraceEventInvoked<TestScriptableList>(i =>
 			{
 				i.Add(1);
@@ -285,12 +240,9 @@ namespace AuroraPunks.ScriptableValues.Tests.Editor
 				i.Add(3);
 				i.Add(4);
 				i.Add(5);
-			}, i =>
-			{
-				i.Sort(0, i.Count, Comparer<int>.Default);
-			});
+			}, i => { i.Sort(0, i.Count, Comparer<int>.Default); });
 		}
-		
+
 		[Test]
 		public void StackTraceEventInvoked_ScriptableList_TrimExcess()
 		{
@@ -301,33 +253,30 @@ namespace AuroraPunks.ScriptableValues.Tests.Editor
 				i.Add(3);
 				i.Add(4);
 				i.Add(5);
-			}, i =>
-			{
-				i.TrimExcess();
-			});
+			}, i => { i.TrimExcess(); });
 		}
-		
+
 		[Test]
 		public void StackTracesReset_ScriptableList()
 		{
-			TestStackTracesWereReset<TestScriptableList>(i =>
-			{
-				i.Add(0);
-			});
+			TestStackTracesWereReset<TestScriptableList>(i => { i.Add(0); });
 		}
-		
+
 		[Test]
 		public void StackTrace_DontCollect_ScriptableList()
 		{
-			TestStackTraceWereNotCollected<TestScriptableList>(i =>
-			{
-				i.Add(0);
-			});
+			TestStackTraceWereNotCollected<TestScriptableList>(i => { i.Add(0); });
 		}
-		
-		private static void TestStackTraceCount<T>(Action<T> action) where T : ScriptableObject, IStackTraceProvider
+
+		[Test]
+		public void StackTrace_DontCollect_Globally_ScriptableList()
 		{
-			T instance = ScriptableObject.CreateInstance<T>();
+			TestStackTraceWereNoCollectGloballyCollected<TestScriptableList>(i => { i.Add(0); });
+		}
+
+		private void TestStackTraceCount<T>(Action<T> action) where T : ScriptableObject, IStackTraceProvider
+		{
+			T instance = CreateInstance<T>();
 
 			IStackTraceProvider provider = instance;
 
@@ -341,75 +290,64 @@ namespace AuroraPunks.ScriptableValues.Tests.Editor
 			}
 
 			Assert.AreEqual(MAX_STACK_TRACE_LENGTH, provider.Invocations.Count);
-
-			Object.DestroyImmediate(instance);
 		}
 
-		private static void TestStackTraceEventInvoked<T>(Action<T> setup, Action<T> invoke) where T : RuntimeScriptableObject, IStackTraceProvider
+		private void TestStackTraceEventInvoked<T>(Action<T> setup, Action<T> invoke) where T : RuntimeScriptableObject, IStackTraceProvider
 		{
-			T instance = ScriptableObject.CreateInstance<T>();
+			T instance = CreateInstance<T>();
 
 			bool eventInvoked = false;
 
 			setup?.Invoke(instance);
-			
+
 			instance.OnStackTraceAdded += () => { eventInvoked = true; };
-		
+
 			invoke.Invoke(instance);
 
-			try
-			{
-
-				Assert.IsTrue(eventInvoked, "Stack trace event was not invoked.");
-			}
-			catch (AssertionException) { }
-			finally
-			{
-				Object.DestroyImmediate(instance);
-			}
+			Assert.IsTrue(eventInvoked, "Stack trace event was not invoked.");
 		}
-		
-		private static void TestStackTracesWereReset<T>(Action<T> invoke) where T : RuntimeScriptableObject, IStackTraceProvider
+
+		private void TestStackTracesWereReset<T>(Action<T> invoke) where T : RuntimeScriptableObject, IStackTraceProvider
 		{
-			T instance = ScriptableObject.CreateInstance<T>();
-		
+			T instance = CreateInstance<T>();
+
 			invoke.Invoke(instance);
 
-			try
-			{
-				Assert.IsTrue(instance.Invocations.Count > 0, "No stack traces have been added.");
-				instance.ResetValues();
-				Assert.AreEqual(0, instance.Invocations.Count, "Stack traces were not cleared.");
-			}
-			catch (AssertionException) { }
-			finally
-			{
-				Object.DestroyImmediate(instance);
-			}
+			Assert.IsTrue(instance.Invocations.Count > 0, "No stack traces have been added.");
+			instance.ResetValues();
+			Assert.AreEqual(0, instance.Invocations.Count, "Stack traces were not cleared.");
 		}
-		
-		private static void TestStackTraceWereNotCollected<T>(Action<T> invoke) where T : RuntimeScriptableObject, IStackTraceProvider
+
+		private void TestStackTraceWereNotCollected<T>(Action<T> invoke) where T : RuntimeScriptableObject, IStackTraceProvider
 		{
-			T instance = ScriptableObject.CreateInstance<T>();
+			T instance = CreateInstance<T>();
 
 			instance.CollectStackTraces = false;
-			
+
 			bool stackTraceAdded = false;
 
-			instance.OnStackTraceAdded += () => stackTraceAdded = true; 
-			
+			instance.OnStackTraceAdded += () => stackTraceAdded = true;
+
 			invoke.Invoke(instance);
 
-			try
-			{
-				Assert.IsFalse(stackTraceAdded, "Stack trace was collected.");
-				Assert.AreEqual(0, instance.Invocations.Count, "Stack trace was collected.");
-			}
-			catch (AssertionException) { }
-			finally
-			{
-				Object.DestroyImmediate(instance);
-			}
+			Assert.IsFalse(stackTraceAdded, "Stack trace was collected.");
+			Assert.AreEqual(0, instance.Invocations.Count, "Stack trace was collected.");
+		}
+
+		private void TestStackTraceWereNoCollectGloballyCollected<T>(Action<T> invoke) where T : RuntimeScriptableObject, IStackTraceProvider
+		{
+			T instance = CreateInstance<T>();
+
+			ScriptableValuesPreferences.CollectStackTraces = false;
+
+			bool stackTraceAdded = false;
+
+			instance.OnStackTraceAdded += () => stackTraceAdded = true;
+
+			invoke.Invoke(instance);
+
+			Assert.IsFalse(stackTraceAdded, "Stack trace was collected.");
+			Assert.AreEqual(0, instance.Invocations.Count, "Stack trace was collected.");
 		}
 	}
 }
