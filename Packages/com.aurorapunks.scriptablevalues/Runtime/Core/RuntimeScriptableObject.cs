9cb5a1be4b8a496d9e5e182554812cae0485e99f
Hertzole <hampusringstrom@gmail.com>
2023-01-10 07:09:54 +0100

    docs: commented RuntimeScriptableObject

--- Packages/com.aurorapunks.scriptablevalues/Runtime/Core/RuntimeScriptableObject.cs
+++ Packages/com.aurorapunks.scriptablevalues/Runtime/Core/RuntimeScriptableObject.cs
@@ -5,8 +5,15 @@ using UnityEditor;
 
 namespace AuroraPunks.ScriptableValues
 {
+	/// <summary>
+	///     Base class for scriptable objects that have values that should be resettable.
+	/// </summary>
 	public abstract partial class RuntimeScriptableObject : ScriptableObject
 	{
+		/// <summary>
+		///     Subscribes to play mode state changed to listen for play mode events.
+		///     <para>When the game is built, it calls the <see cref="ResetValues" /> method.</para>
+		/// </summary>
 		private void OnEnable()
 		{
 #if UNITY_EDITOR
@@ -16,14 +23,25 @@ namespace AuroraPunks.ScriptableValues
 #endif
 		}
 
+		/// <summary>
+		///     Resets the values of the scriptable object.
+		///     <para>Automatically called when the game is started, both in the editor and in the build.</para>
+		/// </summary>
 		public virtual void ResetValues() { }
 
 #if UNITY_EDITOR
+		/// <summary>
+		///     Unsubscribes from play mode state changed to stop listening for play mode events.
+		/// </summary>
 		private void OnDisable()
 		{
 			EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
 		}
 
+		/// <summary>
+		///     Called when the play mode state changes.
+		/// </summary>
+		/// <param name="state">The new play mode state.</param>
 		private void OnPlayModeStateChanged(PlayModeStateChange state)
 		{
 			switch (state)
@@ -45,6 +63,9 @@ namespace AuroraPunks.ScriptableValues
 			OnExitPlayMode();
 		}
 
+		/// <summary>
+		///     Called when the game exits play mode.
+		/// </summary>
 		protected virtual void OnExitPlayMode() { }
 #endif
 	}
