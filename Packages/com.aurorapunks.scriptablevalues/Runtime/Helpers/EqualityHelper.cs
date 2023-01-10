24a6d8b2119f47c25a134f5d3efde88e4d7ab6c4
Hertzole <hampusringstrom@gmail.com>
2023-01-10 09:47:31 +0100

    docs: commented EqualityHelper

--- Packages/com.aurorapunks.scriptablevalues/Runtime/Helpers/EqualityHelper.cs
+++ Packages/com.aurorapunks.scriptablevalues/Runtime/Helpers/EqualityHelper.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 namespace AuroraPunks.ScriptableValues.Helpers
 {
+	/// <summary>
+	///     Internal helper methods for ScriptableValues.
+	/// </summary>
 	internal static class EqualityHelper
 	{
-		public static bool Equals<T>(T left, T right)
+		/// <summary>
+		///     Helper method for checking if two types are the same.
+		///     <para>This takes Unity objects into consideration.</para>
+		/// </summary>
+		/// <param name="x">The first object to compare.</param>
+		/// <param name="y">The second object to compare.</param>
+		/// <typeparam name="T">The type of the object.</typeparam>
+		/// <returns>True if the specified objects are equal; otherwise, false.</returns>
+		public static bool Equals<T>(T x, T y)
 		{
 			// Do special checking on Unity objects.
 			if (typeof(T).IsSubclassOf(typeof(Object)) || typeof(T) == typeof(Object))
 			{
-				Object leftUnityObject = left as Object;
-				Object rightUnityObject = right as Object;
+				Object xUnityObject = x as Object;
+				Object yUnityObject = y as Object;
 
-				return leftUnityObject == rightUnityObject;
+				return xUnityObject == yUnityObject;
 			}
 
-			return EqualityComparer<T>.Default.Equals(left, right);
+			return EqualityComparer<T>.Default.Equals(x, y);
 		}
 
+		/// <summary>
+		///     Helper method for checking if an object is null.
+		///     <para>This takes Unity objects into consideration.</para>
+		/// </summary>
+		/// <param name="obj">The object to check.</param>
+		/// <typeparam name="T">The type of the object.</typeparam>
+		/// <returns>True if the object is null; otherwise false.</returns>
 		public static bool IsNull<T>(T obj)
 		{
 			// Do special checking on Unity objects.
@@ -31,7 +49,7 @@ namespace AuroraPunks.ScriptableValues.Helpers
 
 			return obj == null;
 		}
-		
+
 		/// <summary>
 		///     Helper method to check if the given object is the same type as the provided generic type.
 		/// </summary>
@@ -47,7 +65,7 @@ namespace AuroraPunks.ScriptableValues.Helpers
 				newValue = newValueT;
 				return true;
 			}
-			
+
 #if DEBUG
 			Debug.LogError($"{typeof(TType)} is not assignable from {value.GetType()}.");
 #endif
