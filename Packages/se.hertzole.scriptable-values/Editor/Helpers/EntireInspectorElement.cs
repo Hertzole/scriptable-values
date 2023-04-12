618f8d9bce2b2e6043d7b34716ff8a94f8a8d4d8
Hertzole <hampusringstrom@gmail.com>
2023-04-12 21:51:43 +0200

    fix: scroll bar appearing in editor if you have addressables installed

--- Packages/se.hertzole.scriptable-values/Editor/Helpers/EntireInspectorElement.cs
+++ Packages/se.hertzole.scriptable-values/Editor/Helpers/EntireInspectorElement.cs
@@ -1,36 +1,57 @@
-﻿using UnityEngine.UIElements;
+﻿using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Hertzole.ScriptableValues.Editor
 {
 	public sealed class EntireInspectorElement : VisualElement
 	{
+		private IMGUIContainer imguiContainer;
 		private VisualElement contentViewport;
-		
+
 		public EntireInspectorElement()
 		{
+			style.flexGrow = 1;
+
 			RegisterCallback<GeometryChangedEvent, VisualElement>((_, root) =>
 			{
 				// Only do this if the view port is null to save resources.
 				if (contentViewport == null)
 				{
-					// Find the template container.
-					TemplateContainer rootVisualContainer = FindParent<TemplateContainer>(root);
-					if (rootVisualContainer != null)
+					VisualElement editorElement = FindParent(root, "EditorElement");
+					if (editorElement != null)
 					{
-						// Find the view port element.
-						contentViewport = rootVisualContainer.Q<VisualElement>("unity-content-viewport");
+						imguiContainer = editorElement.Q<IMGUIContainer>();
+						if (imguiContainer != null)
+						{
+							contentViewport = FindParent<TemplateContainer>(root);
+						}
 					}
 				}
 
 				// The viewport exists.
-				if (contentViewport != null)
+				if (contentViewport != null && imguiContainer != null)
 				{
 					// Update the root size to match the entire inspector.
-					root.style.height = contentViewport.resolvedStyle.height - 70;
+					root.style.height = contentViewport.resolvedStyle.height - imguiContainer.resolvedStyle.height - 32;
 				}
 			}, this);
 		}
-		
+
+		private static VisualElement FindParent(VisualElement element, string typeName)
+		{
+			VisualElement parent = element;
+			do
+			{
+				parent = parent.parent;
+				if (parent != null && parent.GetType().Name == typeName)
+				{
+					return parent;
+				}
+			} while (parent != null);
+
+			return null;
+		}
+
 		private static T FindParent<T>(VisualElement element, string name = null) where T : VisualElement
 		{
 			VisualElement parent = element;
