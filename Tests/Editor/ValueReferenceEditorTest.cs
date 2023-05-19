c46a3e0cc8b67e4973e44348a5bac293d598fcbb
Hertzole <hampusringstrom@gmail.com>
2023-05-19 17:14:57 +0200

    actually made tests pass

--- Tests/Editor/ValueReferenceEditorTest.cs
+++ Tests/Editor/ValueReferenceEditorTest.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Application = UnityEngine.Device.Application;
 using Assert = UnityEngine.Assertions.Assert;
 
 namespace Hertzole.ScriptableValues.Tests.Editor
@@ -6,31 +10,32 @@ namespace Hertzole.ScriptableValues.Tests.Editor
 	public sealed class ValueReferenceEditorTest : BaseEditorTest
 	{
 		[Test]
-		public void OnValueChanging_NewValue()
+		public void OnValueChanging_NewValue_EditMode()
 		{
+			Assert.IsFalse(Application.isPlaying);
+			
 			bool eventInvoked = false;
 			
 			ValueReference<int> reference = new ValueReference<int>(0);
 			reference.OnValueChanging += (oldValue, newValue) =>
 			{
-				Assert.AreEqual(0, oldValue);
-				Assert.AreEqual(1, newValue);
-				
 				eventInvoked = true;
 			};
 			
 			reference.SetPreviousValue();
 
-			reference.Value = 1;
+			reference.constantValue = 1;
 			
 			reference.SetEditorValue();
 
-			Assert.IsTrue(eventInvoked);
+			Assert.IsFalse(eventInvoked);
 		}
 		
 		[Test]
-		public void OnValueChanging_SameValue()
+		public void OnValueChanging_SameValue_EditMode()
 		{
+			Assert.IsFalse(Application.isPlaying);
+
 			bool eventInvoked = false;
 			
 			ValueReference<int> reference = new ValueReference<int>(0);
@@ -41,7 +46,7 @@ namespace Hertzole.ScriptableValues.Tests.Editor
 			
 			reference.SetPreviousValue();
 			
-			reference.Value = 0;
+			reference.constantValue = 0;
 			
 			reference.SetEditorValue();
 			
@@ -49,31 +54,32 @@ namespace Hertzole.ScriptableValues.Tests.Editor
 		}
 		
 		[Test]
-		public void OnValueChanged_NewValue()
+		public void OnValueChanged_NewValue_EditMode()
 		{
+			Assert.IsFalse(Application.isPlaying);
+			
 			bool eventInvoked = false;
 			
 			ValueReference<int> reference = new ValueReference<int>(0);
 			reference.OnValueChanged += (oldValue, newValue) =>
 			{
-				Assert.AreEqual(0, oldValue);
-				Assert.AreEqual(1, newValue);
-				
 				eventInvoked = true;
 			};
 			
 			reference.SetPreviousValue();
 
-			reference.Value = 1;
+			reference.constantValue = 1;
 			
 			reference.SetEditorValue();
 			
-			Assert.IsTrue(eventInvoked);
+			Assert.IsFalse(eventInvoked);
 		}
 		
 		[Test]
-		public void OnValueChanged_SameValue()
+		public void OnValueChanged_SameValue_EditMode()
 		{
+			Assert.IsFalse(Application.isPlaying);
+
 			bool eventInvoked = false;
 			
 			ValueReference<int> reference = new ValueReference<int>(0);
@@ -84,10 +90,98 @@ namespace Hertzole.ScriptableValues.Tests.Editor
 			
 			reference.SetPreviousValue();
 			
-			reference.Value = 0;
+			reference.constantValue = 0;
 			
 			reference.SetEditorValue();
 			
+			Assert.IsFalse(eventInvoked);
+		}
+		
+		[UnityTest]
+		public IEnumerator OnValueChanging_NewValue_PlayMode()
+		{
+			yield return new EnterPlayMode(false);
+
+			bool eventInvoked = false;
+
+			ValueReference<int> reference = new ValueReference<int>(0);
+			reference.OnValueChanging += (oldValue, newValue) =>
+			{
+				Assert.AreEqual(0, oldValue);
+				Assert.AreEqual(1, newValue);
+
+				eventInvoked = true;
+			};
+
+			reference.SetPreviousValue();
+
+			reference.constantValue = 1;
+
+			reference.SetEditorValue();
+
+			Assert.IsTrue(eventInvoked);
+		}
+
+		[UnityTest]
+		public IEnumerator OnValueChanging_SameValue_PlayMode()
+		{
+			yield return new EnterPlayMode(false);
+
+			bool eventInvoked = false;
+
+			ValueReference<int> reference = new ValueReference<int>(0);
+			reference.OnValueChanging += (oldValue, newValue) => { eventInvoked = true; };
+
+			reference.SetPreviousValue();
+
+			reference.constantValue = 0;
+
+			reference.SetEditorValue();
+
+			Assert.IsFalse(eventInvoked);
+		}
+
+		[UnityTest]
+		public IEnumerator OnValueChanged_NewValue_PlayMode()
+		{
+			yield return new EnterPlayMode(false);
+
+			bool eventInvoked = false;
+
+			ValueReference<int> reference = new ValueReference<int>(0);
+			reference.OnValueChanged += (oldValue, newValue) =>
+			{
+				Assert.AreEqual(0, oldValue);
+				Assert.AreEqual(1, newValue);
+
+				eventInvoked = true;
+			};
+
+			reference.SetPreviousValue();
+
+			reference.constantValue = 1;
+
+			reference.SetEditorValue();
+
+			Assert.IsTrue(eventInvoked);
+		}
+
+		[UnityTest]
+		public IEnumerator OnValueChanged_SameValue_PlayMode()
+		{
+			yield return new EnterPlayMode(false);
+
+			bool eventInvoked = false;
+
+			ValueReference<int> reference = new ValueReference<int>(0);
+			reference.OnValueChanged += (oldValue, newValue) => { eventInvoked = true; };
+
+			reference.SetPreviousValue();
+
+			reference.constantValue = 0;
+
+			reference.SetEditorValue();
+
 			Assert.IsFalse(eventInvoked);
 		}
 	}
