c33e6ab1866a8d6e3446dc5a61cd562ccbe9237e
Hertzole <hampusringstrom@gmail.com>
2023-06-14 00:43:38 +0200

    add: FishNet networking support for values and lists

---
+++ Packages/se.hertzole.scriptable-values/Runtime/Networking/SyncedScriptableList.cs
@@ -0,0 +1,53 @@
+﻿#if (FISHNET)
+#define SCRIPTABLE_VALUES_NETWORKING
+#endif
+
+#if SCRIPTABLE_VALUES_NETWORKING
+using System;
+
+namespace Hertzole.ScriptableValues
+{
+	public sealed partial class SyncedScriptableList<T> : IDisposable
+	{
+		private ScriptableList<T> targetList;
+
+		public void Initialize(ScriptableList<T> list)
+		{
+			targetList = list;
+
+			targetList.OnAdded += OnAdded;
+			targetList.OnRemoved += OnRemoved;
+			targetList.OnInserted += OnInserted;
+			targetList.OnSet += OnSet;
+			targetList.OnCleared += OnCleared;
+
+			OnInitialized();
+		}
+
+		partial void OnAdded(T item);
+
+		partial void OnRemoved(int index, T item);
+
+		partial void OnInserted(int index, T item);
+
+		partial void OnSet(int index, T oldItem, T newItem);
+
+		partial void OnCleared();
+
+		partial void OnInitialized();
+
+		public void Dispose()
+		{
+			targetList.OnAdded -= OnAdded;
+			targetList.OnRemoved -= OnRemoved;
+			targetList.OnInserted -= OnInserted;
+			targetList.OnSet -= OnSet;
+			targetList.OnCleared -= OnCleared;
+
+			OnDisposed();
+		}
+
+		partial void OnDisposed();
+	}
+}
+#endif
