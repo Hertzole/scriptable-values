892c08b1fcac1b3dd30fa2d82f068c853c7d76d0
Hertzole <hampusringstrom@gmail.com>
2023-06-14 00:43:38 +0200

    add: FishNet networking support for values and lists

---
+++ Runtime/Networking/FishNet/SyncedScriptableList.FishNet.cs
@@ -0,0 +1,132 @@
+﻿#if (FISHNET)
+#define SCRIPTABLE_VALUES_NETWORKING
+#endif
+
+#if SCRIPTABLE_VALUES_NETWORKING && FISHNET
+using System;
+using FishNet.Object.Synchronizing;
+using FishNet.Object.Synchronizing.Internal;
+
+namespace Hertzole.ScriptableValues
+{
+	partial class SyncedScriptableList<T> : SyncBase, ICustomSync
+	{
+		private readonly SyncList<T> syncList = new SyncList<T>();
+
+		public object GetSerializedType()
+		{
+			return null;
+		}
+
+		protected override void Registered()
+		{
+			syncList.InitializeInstance(NetworkBehaviour, byte.MaxValue - SyncIndex, Settings.WritePermission, Settings.ReadPermission, Settings.SendRate,
+				Settings.Channel, IsSyncObject);
+
+			base.Registered();
+			syncList.SetRegistered();
+		}
+
+		partial void OnInitialized()
+		{
+			syncList.OnChange += OnSyncListChanged;
+		}
+
+		private void OnSyncListChanged(SyncListOperation op, int index, T oldItem, T newItem, bool asServer)
+		{
+			if (asServer)
+			{
+				return;
+			}
+
+			switch (op)
+			{
+				case SyncListOperation.Add:
+					targetList.Add(newItem);
+					break;
+				case SyncListOperation.Insert:
+					targetList.Insert(index, newItem);
+					break;
+				case SyncListOperation.Set:
+					targetList[index] = newItem;
+					break;
+				case SyncListOperation.RemoveAt:
+					targetList.RemoveAt(index);
+					break;
+				case SyncListOperation.Clear:
+					targetList.Clear();
+					break;
+				case SyncListOperation.Complete:
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(op), op, null);
+			}
+		}
+
+		partial void OnAdded(T item)
+		{
+			bool asServerInvoke = !IsNetworkInitialized || NetworkBehaviour.IsServer;
+
+			if (!asServerInvoke)
+			{
+				return;
+			}
+
+			syncList.Add(item);
+		}
+
+		partial void OnRemoved(int index, T item)
+		{
+			bool asServerInvoke = !IsNetworkInitialized || NetworkBehaviour.IsServer;
+
+			if (!asServerInvoke)
+			{
+				return;
+			}
+
+			syncList.RemoveAt(index);
+		}
+
+		partial void OnInserted(int index, T item)
+		{
+			bool asServerInvoke = !IsNetworkInitialized || NetworkBehaviour.IsServer;
+
+			if (!asServerInvoke)
+			{
+				return;
+			}
+
+			syncList.Insert(index, item);
+		}
+
+		partial void OnSet(int index, T oldItem, T newItem)
+		{
+			bool asServerInvoke = !IsNetworkInitialized || NetworkBehaviour.IsServer;
+
+			if (!asServerInvoke)
+			{
+				return;
+			}
+
+			syncList[index] = newItem;
+		}
+
+		partial void OnCleared()
+		{
+			bool asServerInvoke = !IsNetworkInitialized || NetworkBehaviour.IsServer;
+
+			if (!asServerInvoke)
+			{
+				return;
+			}
+
+			syncList.Clear();
+		}
+
+		partial void OnDisposed()
+		{
+			syncList.OnChange -= OnSyncListChanged;
+		}
+	}
+}
+#endif
---
+++ Runtime/Networking/SyncedScriptableValue.cs
@@ -0,0 +1,29 @@
+﻿#if (FISHNET)
+#define SCRIPTABLE_VALUES_NETWORKING
+#endif
+
+#if SCRIPTABLE_VALUES_NETWORKING
+using System;
+
+namespace Hertzole.ScriptableValues
+{
+	public sealed partial class SyncedScriptableValue<T> : IDisposable
+	{
+		private ScriptableValue<T> targetValue;
+
+		public void Initialize(ScriptableValue<T> value)
+		{
+			targetValue = value;
+			
+			targetValue.OnValueChanging += OnValueChanging;
+		}
+
+		partial void OnValueChanging(T previousValue, T newValue);
+
+		public void Dispose()
+		{
+			targetValue.OnValueChanging -= OnValueChanging;
+		}
+	}
+}
+#endif
