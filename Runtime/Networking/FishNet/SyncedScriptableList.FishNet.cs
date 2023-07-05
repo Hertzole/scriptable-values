0eaab08eea2700c4f2be7b135abfe30b35ee8784
Hertzole <hampusringstrom@gmail.com>
2023-07-05 22:34:32 +0200

    fix: fishnet collections not working as intended with host client

--- Runtime/Networking/FishNet/SyncedScriptableList.FishNet.cs
+++ Runtime/Networking/FishNet/SyncedScriptableList.FishNet.cs
@@ -11,7 +11,20 @@ namespace Hertzole.ScriptableValues
 {
 	partial class SyncedScriptableList<T> : SyncBase, ICustomSync
 	{
+		private DidOperationFlags didOperation;
+
 		private readonly SyncList<T> syncList = new SyncList<T>();
+
+		[Flags]
+		private enum DidOperationFlags
+		{
+			None = 0,
+			Add = 1,
+			Insert = 2,
+			Set = 4,
+			Remove = 8,
+			Clear = 16
+		}
 
 		protected override void Registered()
 		{
@@ -29,7 +42,7 @@ namespace Hertzole.ScriptableValues
 
 		private void OnSyncListChanged(SyncListOperation op, int index, T oldItem, T newItem, bool asServer)
 		{
-			if (asServer)
+			if (!isInitialized || asServer)
 			{
 				return;
 			}
@@ -37,18 +50,53 @@ namespace Hertzole.ScriptableValues
 			switch (op)
 			{
 				case SyncListOperation.Add:
+					if ((didOperation & DidOperationFlags.Add) != 0)
+					{
+						didOperation &= ~DidOperationFlags.Add;
+						return;
+					}
+
+					didOperation |= DidOperationFlags.Add;
 					targetList.Add(newItem);
 					break;
 				case SyncListOperation.Insert:
+					if ((didOperation & DidOperationFlags.Insert) != 0)
+					{
+						didOperation &= ~DidOperationFlags.Insert;
+						return;
+					}
+
+					didOperation |= DidOperationFlags.Insert;
 					targetList.Insert(index, newItem);
 					break;
 				case SyncListOperation.Set:
+					if ((didOperation & DidOperationFlags.Set) != 0)
+					{
+						didOperation &= ~DidOperationFlags.Set;
+						return;
+					}
+
+					didOperation |= DidOperationFlags.Set;
 					targetList[index] = newItem;
 					break;
 				case SyncListOperation.RemoveAt:
+					if ((didOperation & DidOperationFlags.Remove) != 0)
+					{
+						didOperation &= ~DidOperationFlags.Remove;
+						return;
+					}
+
+					didOperation |= DidOperationFlags.Remove;
 					targetList.RemoveAt(index);
 					break;
 				case SyncListOperation.Clear:
+					if ((didOperation & DidOperationFlags.Clear) != 0)
+					{
+						didOperation &= ~DidOperationFlags.Clear;
+						return;
+					}
+
+					didOperation |= DidOperationFlags.Clear;
 					targetList.Clear();
 					break;
 				case SyncListOperation.Complete:
@@ -60,6 +108,12 @@ namespace Hertzole.ScriptableValues
 
 		partial void OnAdded(T item)
 		{
+			if ((didOperation & DidOperationFlags.Add) != 0)
+			{
+				didOperation &= ~DidOperationFlags.Add;
+				return;
+			}
+
 			bool asServerInvoke = !IsNetworkInitialized || NetworkBehaviour.IsServer;
 
 			if (!asServerInvoke)
@@ -67,11 +121,18 @@ namespace Hertzole.ScriptableValues
 				return;
 			}
 
+			didOperation |= DidOperationFlags.Add;
 			syncList.Add(item);
 		}
 
 		partial void OnRemoved(int index, T item)
 		{
+			if ((didOperation & DidOperationFlags.Remove) != 0)
+			{
+				didOperation &= ~DidOperationFlags.Remove;
+				return;
+			}
+
 			bool asServerInvoke = !IsNetworkInitialized || NetworkBehaviour.IsServer;
 
 			if (!asServerInvoke)
@@ -79,11 +140,18 @@ namespace Hertzole.ScriptableValues
 				return;
 			}
 
+			didOperation |= DidOperationFlags.Remove;
 			syncList.RemoveAt(index);
 		}
 
 		partial void OnInserted(int index, T item)
 		{
+			if ((didOperation & DidOperationFlags.Insert) != 0)
+			{
+				didOperation &= ~DidOperationFlags.Insert;
+				return;
+			}
+
 			bool asServerInvoke = !IsNetworkInitialized || NetworkBehaviour.IsServer;
 
 			if (!asServerInvoke)
@@ -91,11 +159,18 @@ namespace Hertzole.ScriptableValues
 				return;
 			}
 
+			didOperation |= DidOperationFlags.Insert;
 			syncList.Insert(index, item);
 		}
 
 		partial void OnSet(int index, T oldItem, T newItem)
 		{
+			if ((didOperation & DidOperationFlags.Set) != 0)
+			{
+				didOperation &= ~DidOperationFlags.Set;
+				return;
+			}
+
 			bool asServerInvoke = !IsNetworkInitialized || NetworkBehaviour.IsServer;
 
 			if (!asServerInvoke)
@@ -103,11 +178,18 @@ namespace Hertzole.ScriptableValues
 				return;
 			}
 
+			didOperation |= DidOperationFlags.Set;
 			syncList[index] = newItem;
 		}
 
 		partial void OnCleared()
 		{
+			if ((didOperation & DidOperationFlags.Clear) != 0)
+			{
+				didOperation &= ~DidOperationFlags.Clear;
+				return;
+			}
+
 			bool asServerInvoke = !IsNetworkInitialized || NetworkBehaviour.IsServer;
 
 			if (!asServerInvoke)
@@ -115,6 +197,7 @@ namespace Hertzole.ScriptableValues
 				return;
 			}
 
+			didOperation |= DidOperationFlags.Clear;
 			syncList.Clear();
 		}
 
@@ -123,7 +206,7 @@ namespace Hertzole.ScriptableValues
 			DisposeTargetList();
 			syncList.OnChange -= OnSyncListChanged;
 		}
-		
+
 		public object GetSerializedType()
 		{
 			return null;
