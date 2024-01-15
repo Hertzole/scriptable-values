2116dff6197e047e890d48fa7b3493a38d42dcac
Hertzole <hampusringstrom@gmail.com>
2024-01-15 04:24:55 +0100

    ci: maybe fixed fix paths script
    I hate github actions so much

--- ci/fix_paths.sh
+++ ci/fix_paths.sh
@@ -3,6 +3,6 @@ echo "Finding coverage files in $1"
 if [ -d "$1" ]; then
     find $1 -name "*.xml" -type f -print0 | xargs -0 -I {} sh -c '
         echo "Fixing paths in {}"
-        sed -i "s/\/github\/workspace\/g" "{}"
+        sed -i "s/\/github\/workspace\///g" "{}"
     '
 fi
