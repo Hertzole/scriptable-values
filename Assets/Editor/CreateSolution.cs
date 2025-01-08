84db4b0219f331e25cababbffe73d379a4b4a2f1
Hertzole <contact@hertzole.se>
2025-01-08 18:25:30 +0100

    ci: even better sln sync hopefully

---
+++ Assets/Editor/CreateSolution.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using Unity.CodeEditor;
+using UnityEditor;
+
+namespace GitTools
+{
+	public static class Solution
+	{
+		[MenuItem("Git Tools/Manual Sync")]
+		public static void Sync()
+		{
+			AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+			CodeEditor.CurrentEditor.SyncAll();
+		}
+	}
+}
+#endif
