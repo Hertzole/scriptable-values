c70244733dda4a0413ff0da8bd7d485e74093dd3
Hertzole <contact@hertzole.se>
2025-01-10 16:13:39 +0100

    refactor: move NotifyPropertyChanged to base RuntimeScriptableObject

--- Packages/se.hertzole.scriptable-values/Runtime/Values/ScriptableValue.Bindings.cs
+++ Packages/se.hertzole.scriptable-values/Runtime/Values/ScriptableValue.Bindings.cs
@@ -1,41 +1,21 @@
 ﻿#if SCRIPTABLE_VALUES_RUNTIME_BINDING
-using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEngine.UIElements;
 
 namespace Hertzole.ScriptableValues
 {
-	partial class ScriptableValue : IDataSourceViewHashProvider, INotifyBindablePropertyChanged
+	partial class ScriptableValue
 	{
-		private event EventHandler<BindablePropertyChangedEventArgs> OnPropertyChanged;
-
-		event EventHandler<BindablePropertyChangedEventArgs> INotifyBindablePropertyChanged.propertyChanged
-		{
-			add { OnPropertyChanged += value; }
-			remove { OnPropertyChanged -= value; }
-		}
-
-		protected virtual long GetViewHashCode()
+		protected override long GetViewHashCode()
 		{
 			unchecked
 			{
 				long hash = 17;
+				hash = hash * 23 + base.GetViewHashCode();
 				hash = hash * 23 + isReadOnly.GetHashCode();
 				hash = hash * 23 + resetValueOnStart.GetHashCode();
 				hash = hash * 23 + setEqualityCheck.GetHashCode();
 				return hash;
 			}
-		}
-
-		protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
-		{
-			OnPropertyChanged?.Invoke(this, new BindablePropertyChangedEventArgs(propertyName));
-		}
-
-		long IDataSourceViewHashProvider.GetViewHashCode()
-		{
-			return GetViewHashCode();
 		}
 	}
 
