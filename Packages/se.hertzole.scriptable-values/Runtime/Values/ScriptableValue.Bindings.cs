6a37e32ada7f460d895f3c047fdbfd74709aef14
Hertzole <contact@hertzole.se>
2025-01-20 20:12:43 +0100

    refacotr: move generic classes to own file

--- Packages/se.hertzole.scriptable-values/Runtime/Values/ScriptableValue.Bindings.cs
+++ Packages/se.hertzole.scriptable-values/Runtime/Values/ScriptableValue.Bindings.cs
@@ -1,6 +1,4 @@
 ﻿#if SCRIPTABLE_VALUES_RUNTIME_BINDING
-using System.Collections.Generic;
-
 namespace Hertzole.ScriptableValues
 {
 	partial class ScriptableValue
@@ -14,22 +12,6 @@ namespace Hertzole.ScriptableValues
 				hash = hash * 23 + isReadOnly.GetHashCode();
 				hash = hash * 23 + resetValueOnStart.GetHashCode();
 				hash = hash * 23 + setEqualityCheck.GetHashCode();
-				return hash;
-			}
-		}
-	}
-
-	partial class ScriptableValue<T>
-	{
-		protected override long GetViewHashCode()
-		{
-			unchecked
-			{
-				long hash = 17;
-				hash = hash * 23 + base.GetViewHashCode();
-				hash = hash * 23 + EqualityComparer<T>.Default.GetHashCode(Value);
-				hash = hash * 23 + EqualityComparer<T>.Default.GetHashCode(PreviousValue);
-				hash = hash * 23 + EqualityComparer<T>.Default.GetHashCode(DefaultValue);
 				return hash;
 			}
 		}
