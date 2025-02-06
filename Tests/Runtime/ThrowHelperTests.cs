a003cc13ad830fb354b2c5b9f4d8db06725ad04a
Hertzole <contact@hertzole.se>
2025-02-06 17:29:20 +0100

    test: ThrowHelper

---
+++ Tests/Runtime/ThrowHelperTests.cs
@@ -0,0 +1,34 @@
+﻿using Hertzole.ScriptableValues.Helpers;
+using NUnit.Framework;
+
+namespace Hertzole.ScriptableValues.Tests
+{
+	public class ThrowHelperTests
+	{
+		[Test]
+		public void ThrowIfNull_NullObject_ThrowsException()
+		{
+			Assert.Throws<System.ArgumentNullException>(() => ThrowHelper.ThrowIfNull(null, "test"));
+		}
+		
+		[Test]
+		public void ThrowIfNull_NotNullObject_DoesNotThrowException()
+		{
+			Assert.DoesNotThrow(() => ThrowHelper.ThrowIfNull(new object(), "test"));
+		}
+		
+		[Test]
+		public void ThrowIfDisposed_Disposed_ThrowsException()
+		{
+			const bool is_disposed = true;
+			Assert.Throws<System.ObjectDisposedException>(() => ThrowHelper.ThrowIfDisposed(is_disposed));
+		}
+		
+		[Test]
+		public void ThrowIfDisposed_NotDisposed_DoesNotThrowException()
+		{
+			const bool is_disposed = false;
+			Assert.DoesNotThrow(() => ThrowHelper.ThrowIfDisposed(is_disposed));
+		}
+	}
+}
