2d1b1ba93f39910e9e131bce4e3be5e1303dcc97
Hertzole <contact@hertzole.se>
2025-03-13 01:43:45 +0100

    chore: include ThrowHelper changes
    ops

--- Runtime/Helpers/ThrowHelper.cs
+++ Runtime/Helpers/ThrowHelper.cs
@@ -66,6 +66,24 @@ namespace Hertzole.ScriptableValues.Helpers
 			}
 		}
 
+        /// <summary>
+        /// Throws an <see cref="ArgumentOutOfRangeException"/> if the value is outside the specified range.
+        /// </summary>
+        /// <remarks>This only throws in development builds and never release builds.</remarks>
+        /// <param name="paramName">The name of the parameter that was wrong.</param>
+        /// <param name="value">The actual value.</param>
+        /// <param name="min">The minimum value.</param>
+        /// <param name="max">The maximum value.</param>
+        /// <exception cref="ArgumentOutOfRangeException">If <c>value</c> is below <c>min</c> or above <c>max</c>.</exception>
+        [Conditional("DEBUG")]
+		public static void ThrowIfOutOfBounds(string paramName, in int value, in int min, in int max)
+		{
+			if (value < min || value > max)
+			{
+				ThrowArgumentOutOfRangeException(paramName, $"{paramName} must be between {min} and {max} (Was {value}).");
+			}
+		}
+
 #if NETSTANDARD2_1
 		[DoesNotReturn]
 #endif
@@ -96,6 +114,18 @@ namespace Hertzole.ScriptableValues.Helpers
 		private static void ThrowReadOnlyException(object context)
 		{
 			throw new ReadOnlyException($"{context} is marked as read only and cannot be modified at runtime.");
+		}
+		
+		/// <summary>
+		/// Throws an <see cref="ArgumentOutOfRangeException"/> with the specified message.
+		/// </summary>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
+#if NETSTANDARD2_1
+		[DoesNotReturn]
+#endif
+		private static void ThrowArgumentOutOfRangeException(string paramName, string message)
+		{
+			throw new ArgumentOutOfRangeException(paramName, message);
 		}
 	}
 }
