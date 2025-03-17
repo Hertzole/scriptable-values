9b0ca84235e87abd9f53571d639c42b376668290
Hertzole <contact@hertzole.se>
2025-03-17 21:15:00 +0100

    feat: ScriptableList remove methods now use the new event system

---
+++ Packages/se.hertzole.scriptable-values/Tests/Runtime/Helper/BaseEventTracker.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hertzole.ScriptableValues.Tests
+{
+	public abstract class BaseEventTracker : IDisposable
+	{
+		public abstract bool HasBeenInvoked();
+
+		public virtual void Dispose() { }
+	}
+}
--- Packages/se.hertzole.scriptable-values/Tests/Runtime/Helper/InvokeCountContext.cs
+++ Packages/se.hertzole.scriptable-values/Tests/Runtime/Helper/InvokeCountContext.cs
@@ -7,20 +7,32 @@ namespace Hertzole.ScriptableValues.Tests
 		public int invokeCount = 0;
 
 		private readonly Dictionary<string, object> args = new Dictionary<string, object>();
-		
+
 		public void AddArg<T>(string key, T value)
 		{
 			args.Add(key, value);
 		}
-		
+
 		public void SetArg<T>(string key, T value)
 		{
 			args[key] = value;
 		}
-		
+
+		public bool TryGetArg<T>(string key, out T value)
+		{
+			if (args.TryGetValue(key, out object obj))
+			{
+				value = (T) obj;
+				return true;
+			}
+
+			value = default;
+			return false;
+		}
+
 		public T GetArg<T>(string key)
 		{
-			return (T)args[key];
+			return (T) args[key];
 		}
 	}
 }
