97d1c92e276c5cba97c29522ac77011e52ebda4b
Hertzole <contact@hertzole.se>
2025-03-24 17:49:39 +0100

    refactor: create generator just for ScriptableValues

---
+++ Runtime/Attributes/GenerateValueCallbackAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hertzole.ScriptableValues
+{
+	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+	public sealed class GenerateValueCallbackAttribute : Attribute
+	{
+		public GenerateValueCallbackAttribute() : this(ValueCallbackType.Changing) { }
+
+		public GenerateValueCallbackAttribute(ValueCallbackType type) { }
+	}
+	
+	public enum ValueCallbackType
+	{
+		Changing = 0,
+		Changed = 1
+	}
+}
