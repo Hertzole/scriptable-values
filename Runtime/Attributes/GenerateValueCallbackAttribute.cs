c18d6d9e9f08eaff9d7db785822a2d1ffe43998a
Hertzole <contact@hertzole.se>
2025-03-28 17:56:01 +0100

    feat: add TypeNotSupportedForCallbackAttribute diagnostics and analyzer

--- Runtime/Attributes/GenerateValueCallbackAttribute.cs
+++ Runtime/Attributes/GenerateValueCallbackAttribute.cs
@@ -5,11 +5,11 @@ namespace Hertzole.ScriptableValues
 	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
 	public sealed class GenerateValueCallbackAttribute : Attribute
 	{
-		public GenerateValueCallbackAttribute() : this(ValueCallbackType.Changing) { }
+		public GenerateValueCallbackAttribute() : this(ValueCallbackType.Changed) { }
 
 		public GenerateValueCallbackAttribute(ValueCallbackType type) { }
 	}
-	
+
 	public enum ValueCallbackType
 	{
 		Changing = 0,
