a2f648b40c2621ea00180b27309fdab2f77c12dc
Hertzole <contact@hertzole.se>
2025-04-04 17:50:14 +0200

    ci: hopefully fixes generator coverage paths

--- ci/fix_paths.sh
+++ ci/fix_paths.sh
@@ -1,8 +1,13 @@
-echo "Finding coverage files in $1"
-
-if [ -d "$1" ]; then
-    find $1 -name "*.xml" -type f -print0 | xargs -0 -I {} sh -c '
+if [ -d "Coverage/unity" ]; then
+    find Coverage/unity -name "*.xml" -type f -print0 | xargs -0 -I {} sh -c '
         echo "Fixing paths in {}"
         sed -i "s/\/github\/workspace\//.\//g" "{}"
+    '
+fi
+
+if [ -d "Coverage/generator" ]; then
+    find Coverage/generator -name "*.xml" -type f -print0 | xargs -0 -I {} sh -c '
+        echo "Fixing .NET paths in {}"
+        sed -i "s/\/home\/runner\/work\/scriptable-values\/scriptable-values\///g" "{}"
     '
 fi
