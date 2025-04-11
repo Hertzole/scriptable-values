38492b54a3539655eaa3a1e3eb7fa1c8ef206e3a
Hertzole <contact@hertzole.se>
2025-04-12 00:53:38 +0200

    feat: implement ForEach in ScriptableList

---
+++ Packages/se.hertzole.scriptable-values/Tests/Runtime/Collections/List/ScriptableListTests.ForEach.cs
@@ -0,0 +1,33 @@
+using System;
+using NUnit.Framework;
+
+namespace Hertzole.ScriptableValues.Tests
+{
+	partial class ScriptableListTests
+	{
+		[Test]
+		public void ForEach()
+		{
+			// Arrange
+			list.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+			int sum = 0;
+
+			// Act
+			list.ForEach(x => sum += x);
+
+			// Assert
+			Assert.AreEqual(55, sum);
+		}
+
+		[Test]
+		public void ForEach_Validations()
+		{
+			// Arrange
+			list.AddRange(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+			// Act & Assert
+			AssertThrows<ArgumentNullException>(() => list.ForEach(null!));
+			AssertThrows<InvalidOperationException>(() => list.ForEach(x => list.Add(0)));
+		}
+	}
+}
