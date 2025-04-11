1929886f5e87a210274065fbe00c7d86b831b514
Hertzole <contact@hertzole.se>
2025-04-12 00:30:01 +0200

    feat: implement FindLast in ScriptableList

---
+++ Packages/se.hertzole.scriptable-values/Tests/Runtime/Collections/List/ScriptableListTests.FindLast.cs
@@ -0,0 +1,29 @@
+using System;
+using NUnit.Framework;
+
+namespace Hertzole.ScriptableValues.Tests
+{
+	partial class ScriptableListTests
+	{
+		[Test]
+		public void FindLast()
+		{
+			// Arrange
+			list.AddRange(new[] { -1, -2, 3, 4, 5 });
+
+			// Act
+			int result = list.FindLast(x => x > 0);
+
+			// Assert
+			Assert.AreEqual(5, result);
+		}
+
+		[Test]
+		public void FindLast_Validations()
+		{
+			list.AddRange(new[] { -1, -2, 3, 4, 5 });
+
+			AssertThrows<ArgumentNullException>(() => list.FindLast(null!));
+		}
+	}
+}
