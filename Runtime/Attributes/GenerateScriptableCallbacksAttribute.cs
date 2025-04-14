21060c82ba88d5dc72c6a6b7480de2c883e9110f
Hertzole <contact@hertzole.se>
2025-04-14 18:03:31 +0200

    chore: exclude attributes from test coverage

--- Runtime/Attributes/GenerateScriptableCallbacksAttribute.cs
+++ Runtime/Attributes/GenerateScriptableCallbacksAttribute.cs
@@ -1,9 +1,11 @@
 #if UNITY_2022_3_OR_NEWER // Only 2022.3 and newer versions support incremental generators.
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Hertzole.ScriptableValues
 {
 	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+	[ExcludeFromCodeCoverage]
 	public sealed class GenerateScriptableCallbacksAttribute : Attribute { }
 }
 #endif
