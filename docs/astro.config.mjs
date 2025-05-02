f8963aaaf4aa6b2b21f1cff4bf46425a72cb64db
Hertzole <contact@hertzole.se>
2025-05-02 21:03:26 +0200

    docs: fix base path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,26 +3,28 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [starlightLinksValidator()],
-			title: 'Scriptable Values Documentation',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/hertzole/scriptable-values' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Types',
-					autogenerate: { directory: 'types' },
-				}
-			],
-			customCss: [
-				'./src/styles/custom.css'
-			]
-		}),
-	],
+    site: 'https://www.hertzole.se',
+    base: '/scriptable-values/',
+    integrations: [starlight({
+        plugins: [starlightLinksValidator()],
+        title: 'Scriptable Values Documentation',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/hertzole/scriptable-values' }],
+        sidebar: [
+            {
+                label: 'Guides',
+                autogenerate: { directory: 'guides' },
+            },
+            {
+                label: 'Types',
+                autogenerate: { directory: 'types' },
+            }
+        ],
+        customCss: [
+            './src/styles/custom.css'
+        ]
+		}), sitemap()],
 });
