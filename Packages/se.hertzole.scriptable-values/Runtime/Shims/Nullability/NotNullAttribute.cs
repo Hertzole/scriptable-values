b5c23bf00c562e408cadc9447834efe270a842cb
Hertzole <contact@hertzole.se>
2025-05-08 14:22:59 +0200

    fix: NotNullAttribute compiler error

--- Packages/se.hertzole.scriptable-values/Runtime/Shims/Nullability/NotNullAttribute.cs
+++ Packages/se.hertzole.scriptable-values/Runtime/Shims/Nullability/NotNullAttribute.cs
@@ -1,9 +1,12 @@
 #if !NETSTANDARD2_1 && !NET5_0_OR_GREATER
+using System;
+using System.Diagnostics;
+
 namespace System.Diagnostics.CodeAnalysis
 {
 	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue)]
 	[ExcludeFromCodeCoverage]
 	[DebuggerNonUserCode]
-	internal sealed class NotNullAttribute { }
+	internal sealed class NotNullAttribute : Attribute { }
 }
 #endif
