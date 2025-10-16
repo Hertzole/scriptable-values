e34207cb6ed6d5e42389df5db3cf4cbb35a6ba52
Hertzole <contact@hertzole.se>
2025-10-17 00:44:02 +0200

    refactor: player loop is now only a initialization system

--- Runtime/Player Loop/ScriptableValuesPlayerLoop.cs
+++ Runtime/Player Loop/ScriptableValuesPlayerLoop.cs
@@ -1,58 +1,45 @@
+#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.LowLevel;
-using UnityEngine.PlayerLoop;
 
 namespace Hertzole.ScriptableValues
 {
-    internal sealed class ScriptableValuesUpdate { }
-
     internal static class ScriptableValuesPlayerLoop
     {
         private static readonly List<IScriptableValueCallbacks> objectsToEnable = new List<IScriptableValueCallbacks>();
         private static readonly List<IScriptableValueCallbacks> objectsToDisable = new List<IScriptableValueCallbacks>();
 
+        [InitializeOnLoadMethod]
+        private static void InitInEditor()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            // When exiting play mode, we want to call OnScriptableObjectPreDisable and OnScriptableObjectDisable on all registered objects.
+            if (state == PlayModeStateChange.EnteredEditMode)
+            {
+                for (int i = 0; i < objectsToDisable.Count; i++)
+                {
+                    objectsToDisable[i].OnScriptableObjectPreDisable();
+                }
+
+                for (int i = 0; i < objectsToDisable.Count; i++)
+                {
+                    objectsToDisable[i].OnScriptableObjectDisable();
+                }
+
+                objectsToDisable.Clear();
+            }
+        }
+
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
         private static void Init()
         {
-            PlayerLoopSystem defaultLoop = PlayerLoop.GetCurrentPlayerLoop();
-
-            PlayerLoopSystem customUpdate = new PlayerLoopSystem
-            {
-                updateDelegate = OnUpdate,
-                type = typeof(ScriptableValuesUpdate),
-                subSystemList = null
-            };
-
-            PlayerLoopSystem loopWithUpdate = InsertSystemAfter<EarlyUpdate>(in defaultLoop, in customUpdate);
-            PlayerLoop.SetPlayerLoop(loopWithUpdate);
-        }
-
-        public static void RegisterForEnable(IScriptableValueCallbacks obj)
-        {
-            if (!objectsToEnable.Contains(obj))
-            {
-                objectsToEnable.Add(obj);
-            }
-        }
-
-        public static void RegisterForDisable(IScriptableValueCallbacks obj)
-        {
-            if (!objectsToDisable.Contains(obj))
-            {
-                objectsToDisable.Add(obj);
-            }
-        }
-
-        private static void OnUpdate()
-        {
-#if UNITY_EDITOR
-            if (!Application.isPlaying)
-            {
-                return;
-            }
-#endif
-
+            // When the game starts, call OnScriptableObjectPreEnable and OnScriptableObjectEnable on all registered objects.
             for (int i = 0; i < objectsToEnable.Count; i++)
             {
                 objectsToEnable[i].OnScriptableObjectPreEnable();
@@ -63,50 +50,35 @@ namespace Hertzole.ScriptableValues
                 objectsToEnable[i].OnScriptableObjectEnable();
             }
 
-            for (int i = 0; i < objectsToDisable.Count; i++)
-            {
-                objectsToDisable[i].OnScriptableObjectPreDisable();
-            }
-
-            for (int i = 0; i < objectsToDisable.Count; i++)
-            {
-                objectsToDisable[i].OnScriptableObjectDisable();
-            }
-
             objectsToEnable.Clear();
-            objectsToDisable.Clear();
         }
 
-        private static PlayerLoopSystem InsertSystemAfter<T>(in PlayerLoopSystem loopSystem, in PlayerLoopSystem newSystem) where T : struct
+        public static void RegisterForEnable(IScriptableValueCallbacks obj)
         {
-            // Create a new root PlayerLoopSystem
-            PlayerLoopSystem newPlayerLoop = new PlayerLoopSystem
+            // This is a bootstrapper, we can't register for enable if the application is already playing. The object should call those methods directly.
+            if (Application.isPlaying)
             {
-                loopConditionFunction = loopSystem.loopConditionFunction,
-                type = loopSystem.type,
-                updateDelegate = loopSystem.updateDelegate,
-                updateFunction = loopSystem.updateFunction
-            };
-
-            // Create a new list to populate with subsystems, including the custom system
-            List<PlayerLoopSystem> newSubSystemList = new List<PlayerLoopSystem>();
-
-            //Iterate through the subsystems in the existing loop we passed in and add them to the new list
-            if (loopSystem.subSystemList != null)
-            {
-                for (int i = 0; i < loopSystem.subSystemList.Length; i++)
-                {
-                    newSubSystemList.Add(loopSystem.subSystemList[i]);
-                    // If the previously added subsystem is of the type to add after, add the custom system
-                    if (loopSystem.subSystemList[i].type == typeof(T))
-                    {
-                        newSubSystemList.Add(newSystem);
-                    }
-                }
+                throw new InvalidOperationException("Cannot register for enable when the application is already playing.");
             }
 
-            newPlayerLoop.subSystemList = newSubSystemList.ToArray();
-            return newPlayerLoop;
+            if (!objectsToEnable.Contains(obj))
+            {
+                objectsToEnable.Add(obj);
+            }
+        }
+
+        public static void RegisterForDisable(IScriptableValueCallbacks obj)
+        {
+            if (!Application.isPlaying)
+            {
+                throw new InvalidOperationException("Cannot register for disable when the application is not playing.");
+            }
+
+            if (!objectsToDisable.Contains(obj))
+            {
+                objectsToDisable.Add(obj);
+            }
         }
     }
 }
+#endif
