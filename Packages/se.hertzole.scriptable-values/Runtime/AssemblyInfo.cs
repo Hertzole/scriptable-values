1abcfb8cbfbe9daf760f4915910f0600e9237d66
Hertzole <contact@hertzole.se>
2025-11-24 13:22:22 +0100

    fix: don't expose internals to assemblies that may not exist when built

--- Packages/se.hertzole.scriptable-values/Runtime/AssemblyInfo.cs
+++ Packages/se.hertzole.scriptable-values/Runtime/AssemblyInfo.cs
@@ -1,12 +1,16 @@
 ﻿using System.Runtime.CompilerServices;
 #if SCRIPTABLE_VALUES_PROPERTIES
 using Unity.Properties;
-#endif
+#endif // SCRIPTABLE_VALUES_PROPERTIES
 
+#if UNITY_EDITOR
 [assembly: InternalsVisibleTo("Hertzole.ScriptableValues.Editor")]
+#endif // UNITY_EDITOR
+#if UNITY_INCLUDE_TESTS
 [assembly: InternalsVisibleTo("Hertzole.ScriptableValues.Tests")]
 [assembly: InternalsVisibleTo("Hertzole.ScriptableValues.Tests.Editor")]
+#endif // UNITY_INCLUDE_TESTS
 
 #if SCRIPTABLE_VALUES_PROPERTIES
 [assembly: GeneratePropertyBagsForAssembly]
-#endif
+#endif // SCRIPTABLE_VALUES_PROPERTIES
