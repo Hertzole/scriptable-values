d0c12d942bb8df99236946480e7b0ce35edc0048
Hertzole <contact@hertzole.se>
2025-12-09 02:03:09 +0100

    feat: ToNativeArray/List for scriptable lists

--- Runtime/Extensions/CollectionExtensions.cs
+++ Runtime/Extensions/CollectionExtensions.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
+using Unity.Collections;
 
 namespace Hertzole.ScriptableValues
 {
-    internal static class CollectionExtensions
+    public static class CollectionExtensions
     {
-        public static bool TryEnsureCapacity<T>(this IList<T> list, int capacity)
+        internal static bool TryEnsureCapacity<T>(this IList<T> list, int capacity)
         {
             if (list is List<T> genericList)
             {
@@ -24,5 +25,52 @@ namespace Hertzole.ScriptableValues
 
             return false;
         }
+
+        /// <summary>
+        ///     Returns a <see cref="NativeArray{T}" /> that is a copy of this list.
+        /// </summary>
+        /// <remarks>This method supports both <c>struct</c> and <c>unmanaged</c> types.</remarks>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="list">The list to copy.</param>
+        /// <param name="allocator">The allocator to use.</param>
+        /// <returns>An array that is a copy of this list.</returns>
+        public static NativeArray<T> ToNativeArray<T>(this ScriptableList<T> list, Allocator allocator) where T : struct
+        {
+            NativeArray<T> nativeArray = new NativeArray<T>(list.Count, allocator, NativeArrayOptions.UninitializedMemory);
+            for (int i = 0; i < list.Count; i++)
+            {
+                nativeArray[i] = list[i];
+            }
+
+            return nativeArray;
+        }
+
+#if SCRIPTABLE_VALUES_UNITY_COLLECTIONS
+        /// <summary>
+        ///     Returns a <see cref="NativeArray{T}" /> that is a copy of this list.
+        /// </summary>
+        /// <remarks>This method only supports <c>unmanaged</c> types.</remarks>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="list">The list to copy.</param>
+        /// <param name="allocator">The allocator to use.</param>
+        /// <returns>An array that is a copy of this list.</returns>
+        public static NativeArray<T> ToNativeArray<T>(this ScriptableList<T> list, AllocatorManager.AllocatorHandle allocator) where T : unmanaged
+        {
+            return list.list.ToNativeArray(allocator);
+        }
+
+        /// <summary>
+        ///     Returns a <see cref="NativeList{T}" /> that is a copy of this list.
+        /// </summary>
+        /// <remarks>This method only supports <c>unmanaged</c> types.</remarks>
+        /// <typeparam name="T">The type of elements in the list.</typeparam>
+        /// <param name="list">The list to copy.</param>
+        /// <param name="allocator">The allocator to use.</param>
+        /// <returns>A <see cref="NativeList{T}" /> that is a copy of this list.</returns>
+        public static NativeList<T> ToNativeList<T>(this ScriptableList<T> list, AllocatorManager.AllocatorHandle allocator) where T : unmanaged
+        {
+            return list.list.ToNativeList(allocator);
+        }
+#endif
     }
 }
